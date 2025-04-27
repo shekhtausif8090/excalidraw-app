c71df5b2ea816cbc24f7a58ee40973e61ae51d0c
Shekh Tausif <shekhtausif8090@gmail.com>
2025-04-27 15:19:55 +0530

    all good

--- Frontend/src/hooks/useWebSocket.ts
+++ Frontend/src/hooks/useWebSocket.ts
@@ -1,7 +1,7 @@
 // chat-app/client/src/hooks/useWebSocket.ts
 import { useState, useEffect, useRef, useCallback } from "react";
 
-const WEBSOCKET_URL = "wss://canvas-flow-backend.onrender.com" 
+//const WEBSOCKET_URL = "wss://canvas-flow-backend.onrender.com" 
 
 const RECONNECT_DELAY = 3000;
 const MAX_RECONNECT_ATTEMPTS = 5;
